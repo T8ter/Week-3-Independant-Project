1362ec961211292d005948ff152ab1ac78467385
Student <example@example.com>
2019-01-25 11:38:32 -0800

    add a javascript for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+function changeNum(number) {
+  for (var i = 0; i < number.length; i++) {
+  }
+}
+
+
+
+
+
+
+
 $(document).ready(function(event) {
   $("form#numberForm").submit(function(event) {
     event.preventDefault();
     var number = $("input#numberInput").val();
-    console.log(number)
+    changeNum(number)
   });
 });
